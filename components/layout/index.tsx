264db95c026983dda84d3db54ae83e45a039eff9
phanchithien <phanchithien456@gmail.com>
2023-08-28 23:10:42 +0700

    fix scale responsive

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -6,7 +6,7 @@ import Navbar from 'react-bootstrap/Navbar';
 
 export default function NavbarHeader() {
     return (
-        <div className="bg-white w-full flex justify-between items-center md:h-18 sm:h-20 fixed top-0 z-50">
+        <div className="bg-white w-full flex justify-between items-center lg:h-16 md:h-18 sm:h-30 fixed top-0 z-50">
             <Navbar expand="lg" className="px-3 md:w-2/5">
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
@@ -28,9 +28,12 @@ export default function NavbarHeader() {
             <Navbar.Brand href="/" className="flex justify-center md:w-1/5">
                 <img src="/logo.jpg" className="h-12" alt="logo" />
             </Navbar.Brand>
-            <Navbar.Brand href="/" className="flex justify-end md:w-2/5 px-3">
-                <img src="/search.png" className="h-4" alt="search" />
-            </Navbar.Brand>
+            <div className="flex justify-end md:w-2/5 pr-3">
+                <Nav.Link href="/" className="md:w-1/8 ">
+                    <img src="/search.png" className="h-4" alt="search" />
+                </Nav.Link>
+            </div>
+
         </div>
     )
 }
